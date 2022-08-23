1bd18958e6cf79a7fba7cc85635cb3c67d0104cd
Anastasiia Davydova <logzkada@gmail.com>
2022-08-23 15:31:13 +0300

    Added Login Page.

---
+++ src/views/Manager.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="manager">
+    <h1>This is a manager page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'manager-view',
+  components: {
+  },
+};
+</script>
